a958174be3d08ae48b1aede5d2e57ab2eea4af79
Mario <mario_scipio@hotmail.es>
2022-04-21 16:16:20 +0200

    Added death animation
    Added death animation for player and enemies. It wil trigger when the character health points fall to 0.

--- RPG-Thing/Assets/Scripts/Combat/Health.cs
+++ RPG-Thing/Assets/Scripts/Combat/Health.cs
@@ -1,15 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace RPG.Combat
 {
     class Health : MonoBehaviour
     {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
+        bool isDead = false;        
 
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            print(healthPoints);
+            if (healthPoints == 0 && !isDead)
+                Die();
+        }
+
+        private void Die()
+        {
+            GetComponent<Animator>().SetTrigger("die");
+            isDead = true;
         }
     }
 }

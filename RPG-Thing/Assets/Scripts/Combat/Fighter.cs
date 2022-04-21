2ab25ebc6024dd90ba0d9bcf8b2e827a7aa17c8f
Mario <mario_scipio@hotmail.es>
2022-04-21 11:04:43 +0200

    Added attack speed
    Added attack speed so a certain amount of time has to pass between attacks

--- RPG-Thing/Assets/Scripts/Combat/Fighter.cs
+++ RPG-Thing/Assets/Scripts/Combat/Fighter.cs
@@ -7,11 +7,15 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float attackSpeed = 1f;
 
         Transform target;
+        float timeSinceLastAttack;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!isInRange())
@@ -27,7 +31,9 @@ namespace RPG.Combat
 
         private void AttackBehaviour()
         {
+            if (timeSinceLastAttack < attackSpeed) return;
             GetComponent<Animator>().SetTrigger("attack");
+            timeSinceLastAttack = 0;          
         }
 
         private bool isInRange()

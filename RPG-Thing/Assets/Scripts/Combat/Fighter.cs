60e2e055b4df53eceede7412a24db8ffdb8c0481
Mario <mario_scipio@hotmail.es>
2022-09-15 11:51:08 +0200

    Sword scriptable object and animation override
    Made a sword scriptable object that contains the sword prefab and the sword animation.
    Equipping a sword overrides the default attack animation and uses the sword attack animation instead

--- RPG-Thing/Assets/Scripts/Combat/Fighter.cs
+++ RPG-Thing/Assets/Scripts/Combat/Fighter.cs
@@ -9,8 +9,8 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float attackSpeed = 1f;
         [SerializeField] float weaponDamage = 5f;
-        [SerializeField] GameObject weaponPrefab = null;
         [SerializeField] Transform handTransform = null;
+        [SerializeField] Weapon weapon = null;
 
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
@@ -93,7 +93,9 @@ namespace RPG.Combat
 
         private void SpawnWeapon()
         {
-            Instantiate(weaponPrefab, handTransform);
+            if (weapon == null) return;
+            Animator animator = GetComponent<Animator>();
+            weapon.Spawn(handTransform, animator);            
         }
     }
 }
